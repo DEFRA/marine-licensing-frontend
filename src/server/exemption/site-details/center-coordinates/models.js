f7d76cccff4d34cf0b10d8d0b72065a384ee134b
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-06-02 18:13:11 +0100

    Move JOI errors to constants

--- src/server/exemption/site-details/center-coordinates/models.js
+++ src/server/exemption/site-details/center-coordinates/models.js
@@ -1,14 +1,5 @@
+import { JOI_ERRORS } from '~/src/server/common/constants/joi.js'
 import joi from 'joi'
-
-const JOI_ERRORS = {
-  NUMBER_BASE: 'number.base',
-  NUMBER_POSITIVE: 'number.positive',
-  NUMBER_RANGE: 'number.range',
-  NUMBER_DECIMAL: 'number.decimal',
-  STRING_EMPTY: 'string.empty',
-  STRING_PATTERN_BASE: 'string.pattern.base',
-  ANY_REQUIRED: 'any.required'
-}
 
 const MIN_LATITUDE = -90
 const MAX_LATITUDE = 90

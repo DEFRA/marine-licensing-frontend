6168dbc27335f37f31c7095e2a88d2a58fe47aae
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-06-03 16:24:12 +0100

    Move schemas into there own files

--- src/server/exemption/site-details/center-coordinates/schemas/osgb36.test.js
+++ src/server/exemption/site-details/center-coordinates/schemas/osgb36.test.js
@@ -1,115 +1,13 @@
-import {
-  wgs84ValidationSchema,
-  osgb36ValidationSchema
-} from '~/src/server/exemption/site-details/center-coordinates/models.js'
-import { mockExemption } from '~/src/server/test-helpers/mocks.js'
+import { osgb36ValidationSchema } from '~/src/server/exemption/site-details/center-coordinates/schemas/osgb36.js'
 import { COORDINATE_SYSTEMS } from '~/src/server/common/constants/exemptions.js'
 
 const mockCoordinates = {
-  [COORDINATE_SYSTEMS.WGS84]: {
-    latitude: mockExemption.siteDetails.coordinates.latitude,
-    longitude: mockExemption.siteDetails.coordinates.longitude
-  },
   [COORDINATE_SYSTEMS.OSGB36]: { eastings: '425053', northings: '564180' }
 }
 
 describe('#centerCoordinate models', () => {
   beforeEach(() => {
     jest.resetAllMocks()
-  })
-
-  describe('#wgs84ValidationSchema model', () => {
-    test('Should correctly validate on valid data', () => {
-      const request = {
-        latitude: mockExemption.siteDetails.coordinates.latitude,
-        longitude: mockExemption.siteDetails.coordinates.longitude
-      }
-
-      const result = wgs84ValidationSchema.validate(request)
-
-      expect(result.error).toBeUndefined()
-    })
-
-    test('Should correctly validate on empty data', () => {
-      const request = {}
-
-      const result = wgs84ValidationSchema.validate(request, {
-        abortEarly: false
-      })
-
-      expect(result.error.message).toContain('LATITUDE_REQUIRED')
-      expect(result.error.message).toContain('LONGITUDE_REQUIRED')
-    })
-
-    test('Should correctly validate when latitude and longitude are empty strings', () => {
-      const request = {
-        latitude: '',
-        longitude: ''
-      }
-
-      const result = wgs84ValidationSchema.validate(request, {
-        abortEarly: false
-      })
-
-      expect(result.error.message).toContain('LATITUDE_REQUIRED')
-      expect(result.error.message).toContain('LONGITUDE_REQUIRED')
-    })
-
-    test('Should correctly validate when latitude and longitude is below minimum allowed value', () => {
-      const request = {
-        latitude: '-91',
-        longitude: '-181'
-      }
-
-      const result = wgs84ValidationSchema.validate(request, {
-        abortEarly: false
-      })
-
-      expect(result.error.message).toContain('LATITUDE_LENGTH')
-      expect(result.error.message).toContain('LONGITUDE_LENGTH')
-    })
-
-    test('Should correctly validate when latitude and longitude is above maximum allowed value', () => {
-      const request = {
-        latitude: '91',
-        longitude: '181'
-      }
-
-      const result = wgs84ValidationSchema.validate(request, {
-        abortEarly: false
-      })
-
-      expect(result.error.message).toContain('LATITUDE_LENGTH')
-      expect(result.error.message).toContain('LONGITUDE_LENGTH')
-    })
-
-    test('Should correctly validate when latitude and longitude have incorrect characters', () => {
-      const request = {
-        latitude: '-9/',
-        longitude: '-18/'
-      }
-
-      const result = wgs84ValidationSchema.validate(request, {
-        abortEarly: false
-      })
-
-      expect(result.error.message).toContain('LATITUDE_NON_NUMERIC')
-      expect(result.error.message).toContain('LONGITUDE_NON_NUMERIC')
-    })
-
-    test('Should correctly validate eastings and northing do not have correct decimal places', () => {
-      const request = {
-        latitude: '50.12345',
-        longitude: '50.12345'
-      }
-
-      const result = wgs84ValidationSchema.validate(request, {
-        abortEarly: false
-      })
-
-      expect(result.error.message).toContain('LATITUDE_DECIMAL_PLACES')
-      expect(result.error.message).toContain('LONGITUDE_DECIMAL_PLACES')
-    })
   })
 
   describe('#osgb36ValidationSchema model', () => {

cbdfc3c4e68640e3b392ede481bfb2bcc6676bd3
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-06-11 11:30:49 +0100

    Back functionality from task list

--- src/server/exemption/site-details/review-site-details/controller.js
+++ src/server/exemption/site-details/review-site-details/controller.js
@@ -7,7 +7,8 @@ import { config } from '~/src/config/config.js'
 import {
   getCoordinateSystemText,
   getReviewSummaryText,
-  getCoordinateDisplayText
+  getCoordinateDisplayText,
+  getSiteDetailsBackLink
 } from './utils.js'
 import Boom from '@hapi/boom'
 import Wreck from '@hapi/wreck'
@@ -17,8 +18,7 @@ export const REVIEW_SITE_DETAILS_VIEW_ROUTE =
 
 const reviewSiteDetailsPageData = {
   pageTitle: 'Review site details',
-  heading: 'Review site details',
-  backLink: routes.WIDTH_OF_SITE
+  heading: 'Review site details'
 }
 
 /**
@@ -27,6 +27,8 @@ const reviewSiteDetailsPageData = {
  */
 export const reviewSiteDetailsController = {
   handler(request, h) {
+    const previousPage = request.headers?.referer
+
     const exemption = getExemptionCache(request)
     const { coordinateSystem } = getCoordinateSystem(request)
 
@@ -43,6 +45,7 @@ export const reviewSiteDetailsController = {
 
     return h.view(REVIEW_SITE_DETAILS_VIEW_ROUTE, {
       ...reviewSiteDetailsPageData,
+      backLink: getSiteDetailsBackLink(previousPage),
       projectName: exemption.projectName,
       summaryData
     })

ba41392d55aa1785bedfb3e608b3c4d62ad00875
Jonny Wyatt <jon.wyatt@equalexperts.com>
2025-09-04 09:58:24 +0100

    Hide back link if JS not enabled

--- src/server/common/components/back-link-history/back-link-history.test.js
+++ src/server/common/components/back-link-history/back-link-history.test.js
@@ -7,6 +7,7 @@ describe('backLinkHistory Component', () => {
   test('Should render back link correctly with default text', () => {
     $backLink = renderComponent('back-link-history')
 
+    expect($backLink('nav').hasClass('hide-if-no-js')).toBe(true)
     expect($backLink('a')).toHaveLength(1)
     expect($backLink('a').text().trim()).toBe('Back')
     expect($backLink('a').attr('href')).toBe('javascript:window.history.back()')

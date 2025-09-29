8b38805c6f5bce4e8829f32098ceab341dfd5c15
Jonny Wyatt <jon.wyatt@equalexperts.com>
2025-09-29 14:27:45 +0100

    Code comment for relationships array

--- src/server/common/plugins/auth/open-id-provider.js
+++ src/server/common/plugins/auth/open-id-provider.js
@@ -25,6 +25,10 @@ export const openIdProvider = async (name) => {
       const displayName = [payload.firstName, payload.lastName]
         .filter((part) => part)
         .join(' ')
+      // destructure the relationships array eg
+      // 81d48d6c-6e94-f011-b4cc-000d3ac28f39:27d48d6c-6e94-f011-b4cc-000d3ac28f39:CDP Child Org 1:0:Employee:0
+      // which is colon-separated with the following parts:
+      // relationshipId:organisationId:organisationName:organisationLoa:relationship:relationshipLoa.
       const [
         _relationshipId,
         applicantOrganisationId,

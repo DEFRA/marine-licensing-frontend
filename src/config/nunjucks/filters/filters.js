e9e6c57c9cbf1df0a05087c41409fe3570069722
Jonny Wyatt <jon.wyatt@equalexperts.com>
2025-10-01 15:07:06 +0100

    Don't export assign from globals

--- src/config/nunjucks/filters/filters.js
+++ src/config/nunjucks/filters/filters.js
@@ -1,4 +1,3 @@
 export { formatDate } from '~/src/config/nunjucks/filters/format-date.js'
 export { formatCurrency } from '~/src/config/nunjucks/filters/format-currency.js'
 export { sanitiseFilename } from '~/src/config/nunjucks/filters/sanitise-filename.js'
-export { default as assign } from 'lodash/assign.js'

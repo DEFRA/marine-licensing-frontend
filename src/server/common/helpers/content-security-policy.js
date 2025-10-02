705ea6ba0931bfedb1dfe923a38243f9e22170a3
Jonny Wyatt <jon.wyatt@equalexperts.com>
2025-10-02 11:23:35 +0100

    Add comment to CSP for script-src hash

--- src/server/common/helpers/content-security-policy.js
+++ src/server/common/helpers/content-security-policy.js
@@ -22,6 +22,8 @@ const contentSecurityPolicy = {
       'manifest-src': "'self'",
       'media-src': "'self'",
       'object-src': "'none'",
+      // Hash 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=' is to support a GOV.UK frontend script bundled within Nunjucks macros
+      // https://frontend.design-system.service.gov.uk/import-javascript/#if-our-inline-javascript-snippet-is-blocked-by-a-content-security-policy
       'script-src':
         "'self' 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='",
       'style-src': "'self'"

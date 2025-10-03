96b6fd5a741cccfdb77079f6e9c69f25a9c1d0e8
Jonny Wyatt <jon.wyatt@equalexperts.com>
2025-10-03 10:08:55 +0100

    Set config - pool to threads to improve speed

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    pool: 'threads',
     setupFiles: ['.vite/setup-files.js', 'allure-vitest/setup'],
     include: ['**/src/**/*.test.js', '**/tests/**/*.test.js'],
     exclude: [

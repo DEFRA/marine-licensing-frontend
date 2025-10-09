2cb894901ec435fb4f7da95f52cc9be64645139b
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-10-09 11:55:23 +0100

    Fix Lint

--- src/server/exemption/site-details/site-name/controller.test.js
+++ src/server/exemption/site-details/site-name/controller.test.js
@@ -5,10 +5,12 @@ import {
   SITE_NAME_VIEW_ROUTE,
   errorMessages
 } from '#src/server/exemption/site-details/site-name/controller.js'
-import { mockExemption as mockExemptionData } from '#src/server/test-helpers/mocks.js'
+import {
+  mockExemption as mockExemptionData,
+  createMockRequest
+} from '#src/server/test-helpers/mocks.js'
 import { routes } from '#src/server/common/constants/routes.js'
 import * as cacheUtils from '#src/server/common/helpers/session-cache/utils.js'
-import { createMockRequest } from '#src/server/test-helpers/mocks.js'
 
 vi.mock('~/src/server/common/helpers/session-cache/utils.js')
 

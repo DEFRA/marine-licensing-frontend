8bdf649dfbca66ba4621c4b2a5f7cde13f204603
Jon Shipley <jon.shipley@esynergy.co.uk>
2025-10-20 13:14:47 +0100

    Update copy and tests

--- src/server/exemption/site-details/review-site-details/controller.test.js
+++ src/server/exemption/site-details/review-site-details/controller.test.js
@@ -375,8 +375,7 @@ describe('#reviewSiteDetails', () => {
               activityDescription: 'Test activity description',
               method:
                 'Manually enter one set of coordinates and a width to create a circular site',
-              coordinateSystem:
-                'OSGB36 (National Grid)\nEastings and Northings',
+              coordinateSystem: 'British National Grid (OSGB36)\nEastings and Northings',
               coordinates: `${mockCoordinates[COORDINATE_SYSTEMS.OSGB36].eastings}, ${mockCoordinates[COORDINATE_SYSTEMS.OSGB36].northings}`,
               width: '100 metres',
               showActivityDates: true,
@@ -1005,7 +1004,9 @@ describe('#reviewSiteDetails', () => {
         test('should return OSGB36 text', () => {
           const result = getCoordinateSystemText(COORDINATE_SYSTEMS.OSGB36)
 
-          expect(result).toBe('OSGB36 (National Grid)\nEastings and Northings')
+          expect(result).toBe(
+            'British National Grid (OSGB36)\nEastings and Northings'
+          )
         })
 
         test('should handle null coordinate system', () => {

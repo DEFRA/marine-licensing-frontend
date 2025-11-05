262eee50d4f2f6a157730eb20a5e42d64dd2ff1a
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-11-05 17:37:04 +0000

    Set maxCookieSize to enforce cache usage

--- src/server/common/helpers/session-cache/session-cache.js
+++ src/server/common/helpers/session-cache/session-cache.js
@@ -7,6 +7,7 @@ export const sessionCache = {
   plugin: yar,
   options: {
     name: sessionConfig.cache.name,
+    maxCookieSize: 0,
     cache: {
       cache: sessionConfig.cache.name,
       expiresIn: sessionConfig.cache.ttl

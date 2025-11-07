506394d35349c38569a8f6a74ffad1eddc1f99cd
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-11-07 14:18:50 +0000

    Add missing await after merge

--- src/server/exemption/activity-dates/controller.js
+++ src/server/exemption/activity-dates/controller.js
@@ -188,9 +188,9 @@ export const activityDatesSubmitController = {
 
       if (action) {
         if (hasSameActivityDatesAcrossSites) {
-          copySameActivityDatesToAllSites(request)
+          await copySameActivityDatesToAllSites(request, h)
         }
-        await saveSiteDetailsToBackend(request)
+        await saveSiteDetailsToBackend(request, h)
       }
 
       return h.redirect(nextRoute)

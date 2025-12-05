774f6e9e8dad513e4272754eeb290e281f1c8075
Jonny Wyatt <jon.wyatt@equalexperts.com>
2025-12-05 11:38:49 +0000

    Log in home controller when redirected to dashboard

--- src/server/common/helpers/check-request-referrer.js
+++ src/server/common/helpers/check-request-referrer.js
@@ -5,11 +5,23 @@ export const isUserReferredFromDefraAccount = (request) => {
 
   const referer = request.headers.referer
 
-  return Boolean(referer?.startsWith(accountManagementUrl))
+  const result = Boolean(referer?.startsWith(accountManagementUrl))
+  if (result) {
+    request.logger.info(
+      `User has come from Defra account. Referer header: ${request.headers.referer}`
+    )
+  }
+  return result
 }
 
 export const isUserReferredFromSignIn = (request) => {
   const referer = request.headers.referer
 
-  return Boolean(referer?.includes('/signin-oidc'))
+  const result = Boolean(referer?.includes('/signin-oidc'))
+  if (result) {
+    request.logger.info(
+      `User has come from signin. Referer header: ${request.headers.referer}`
+    )
+  }
+  return result
 }

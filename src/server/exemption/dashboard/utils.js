7913f1ea9a43e7fa4e56581e523c09c994fbc3a8
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-01-13 13:15:43 +0000

    fix potential xss

--- src/server/exemption/dashboard/utils.js
+++ src/server/exemption/dashboard/utils.js
@@ -1,21 +1,23 @@
 import { formatDate } from '#src/config/nunjucks/filters/format-date.js'
 import { routes } from '#src/server/common/constants/routes.js'
 import { EXEMPTION_TYPE } from '#src/server/common/constants/exemptions.js'
+import escapeHtml from 'lodash/escape.js'
 
 export const getActionButtons = (project) => {
   const isOwnProject = project.isOwnProject ?? true
+  const escapedProjectName = escapeHtml(project.projectName)
 
   if (isOwnProject) {
     if (project.status === 'Draft') {
-      return `<a href="${routes.TASK_LIST}/${project.id}" class="govuk-link govuk-!-margin-right-4 govuk-link--no-visited-state" aria-label="Continue to task list">Continue</a><a href="${routes.DELETE_EXEMPTION}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="Delete ${project.projectName}">Delete</a>`
+      return `<a href="${routes.TASK_LIST}/${project.id}" class="govuk-link govuk-!-margin-right-4 govuk-link--no-visited-state" aria-label="Continue to task list">Continue</a><a href="${routes.DELETE_EXEMPTION}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="Delete ${escapedProjectName}">Delete</a>`
     }
-    return `<a href="${routes.VIEW_DETAILS}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="View details of ${project.projectName}">View details</a>`
+    return `<a href="${routes.VIEW_DETAILS}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="View details of ${escapedProjectName}">View details</a>`
   }
 
   if (project.status === 'Draft') {
     return ''
   }
-  return `<a href="${routes.VIEW_DETAILS}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="View details of ${project.projectName}">View details</a>`
+  return `<a href="${routes.VIEW_DETAILS}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="View details of ${escapedProjectName}">View details</a>`
 }
 
 export const formatProjectsForDisplay = (projects, isEmployee = false) =>
@@ -30,7 +32,7 @@ export const formatProjectsForDisplay = (projects, isEmployee = false) =>
       { text: EXEMPTION_TYPE },
       { text: project.applicationReference || '-' },
       {
-        html: `<strong class="govuk-tag ${tagClass}">${project.status}</strong>`
+        html: `<strong class="govuk-tag ${tagClass}">${escapeHtml(project.status)}</strong>`
       },
       {
         text: project.submittedAt
@@ -43,7 +45,7 @@ export const formatProjectsForDisplay = (projects, isEmployee = false) =>
       baseRow.push({ text: project.ownerName || '-' })
     }
 
-    baseRow.push({ html: getActionButtons(project, isEmployee) })
+    baseRow.push({ html: getActionButtons(project) })
 
     return {
       cells: baseRow,

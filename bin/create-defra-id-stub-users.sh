276440338a3c22b8b5111971f61d4732d7e7e56b
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-01-19 13:47:47 +0000

    Add more users - agent and individual

--- bin/create-defra-id-stub-users.sh
+++ bin/create-defra-id-stub-users.sh
@@ -4,5 +4,7 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
 
-curl -H "Content-Type: application/json" -X POST -d @"$SCRIPT_DIR"/users/some-org-emp-1.json http://localhost:3200/cdp-defra-id-stub/API/register | jq
-curl -H "Content-Type: application/json" -X POST -d @"$SCRIPT_DIR"/users/some-org-emp-2.json http://localhost:3200/cdp-defra-id-stub/API/register | jq
+curl -sS -H "Content-Type: application/json" -X POST -d @"$SCRIPT_DIR"/users/some-org-emp-1.json http://localhost:3200/cdp-defra-id-stub/API/register | jq
+curl -sS -H "Content-Type: application/json" -X POST -d @"$SCRIPT_DIR"/users/some-org-emp-2.json http://localhost:3200/cdp-defra-id-stub/API/register | jq
+curl -sS -H "Content-Type: application/json" -X POST -d @"$SCRIPT_DIR"/users/some-individual-1.json http://localhost:3200/cdp-defra-id-stub/API/register | jq
+curl -sS -H "Content-Type: application/json" -X POST -d @"$SCRIPT_DIR"/users/some-agent-1.json http://localhost:3200/cdp-defra-id-stub/API/register | jq

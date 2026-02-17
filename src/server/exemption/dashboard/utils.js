ee17095ceead5885372aaa44da3dfcd8fed7aafc
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-02-17 17:20:57 +0000

    Small refactor to use constants

--- src/server/exemption/dashboard/utils.js
+++ src/server/exemption/dashboard/utils.js
@@ -1,6 +1,9 @@
 import { formatDate } from '#src/config/nunjucks/filters/format-date.js'
 import { routes } from '#src/server/common/constants/routes.js'
-import { EXEMPTION_TYPE } from '#src/server/common/constants/exemptions.js'
+import {
+  EXEMPTION_TYPE,
+  EXEMPTION_STATUS
+} from '#src/server/common/constants/exemptions.js'
 import { getTagStyle } from '#src/server/common/helpers/exemptions/get-tag-style.js'
 import escapeHtml from 'lodash/escape.js'
 
@@ -19,10 +22,10 @@ export const getActionButtons = (project) => {
 
   const escapedProjectName = escapeHtml(projectName)
 
-  const canWithdraw = status === 'Active' && !!isOwnProject
+  const canWithdraw = status === EXEMPTION_STATUS.ACTIVE && !!isOwnProject
 
   if (isOwnProject) {
-    if (status === 'Draft') {
+    if (status === EXEMPTION_STATUS.DRAFT) {
       return `<a href="${routes.TASK_LIST}/${id}" class="govuk-link govuk-!-margin-right-4 govuk-link--no-visited-state" aria-label="Continue to task list">Continue</a><a href="${routes.DELETE_EXEMPTION}/${id}" class="govuk-link govuk-link--no-visited-state" aria-label="Delete ${escapedProjectName}">Delete</a>`
     }
     const marginClass = canWithdraw
@@ -37,7 +40,7 @@ export const getActionButtons = (project) => {
     return buttons
   }
 
-  if (project.status === 'Draft') {
+  if (project.status === EXEMPTION_STATUS.DRAFT) {
     return ''
   }
   return `<a href="${routes.VIEW_DETAILS}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="View details of ${escapedProjectName}">View details</a>`

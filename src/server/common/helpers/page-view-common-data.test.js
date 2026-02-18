8b2f9d4a6bfdd87b48be614883aa6b9f26e43362
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-02-18 17:09:26 +0000

    Add link to home page

--- src/server/common/helpers/page-view-common-data.test.js
+++ src/server/common/helpers/page-view-common-data.test.js
@@ -1,4 +1,7 @@
-import { getPageViewCommonData } from './page-view-common-data.js'
+import {
+  changeOrganisationLinkRoutes,
+  getPageViewCommonData
+} from './page-view-common-data.js'
 import { getUserSession } from '#src/server/common/plugins/auth/utils.js'
 import { routes } from '#src/server/common/constants/routes.js'
 
@@ -21,21 +24,32 @@ describe('getPageViewCommonData', () => {
     expect(result).toEqual({})
   })
 
-  test('should return showChangeOrganisationLink false when not on dashboard page', async () => {
+  test('should return showChangeOrganisationLink false for correct pages', async () => {
     const mockUserSession = {
       organisationName: 'Test Organisation Ltd',
       hasMultipleOrgPickerEntries: false
     }
     mockGetUserSession.mockResolvedValue(mockUserSession)
 
-    const mockRequest = {
+    const mockRequestDashboardPage = {
       state: { userSession: 'mock-session' },
       path: routes.TASK_LIST
     }
 
-    const result = await getPageViewCommonData(mockRequest)
+    const resultDashboard = await getPageViewCommonData(
+      mockRequestDashboardPage
+    )
 
-    expect(result.showChangeOrganisationLink).toEqual(false)
+    expect(resultDashboard.showChangeOrganisationLink).toEqual(false)
+
+    const mockRequestHomePage = {
+      state: { userSession: 'mock-session' },
+      path: routes.SERVICE_HOME
+    }
+
+    const resultHomePage = await getPageViewCommonData(mockRequestHomePage)
+
+    expect(resultHomePage.showChangeOrganisationLink).toEqual(false)
   })
 
   test('should return showChangeOrganisationLink false when user has single organisation on dashboard', async () => {

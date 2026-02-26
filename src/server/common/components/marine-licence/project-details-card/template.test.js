70aa387568ca8ec0bd8511d4ac574be7f257d5b1
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-02-26 10:16:46 +0000

    Tidy test

--- src/server/common/components/marine-licence/project-details-card/template.test.js
+++ src/server/common/components/marine-licence/project-details-card/template.test.js
@@ -3,28 +3,24 @@ import { renderComponent } from '#src/server/test-helpers/component-helpers.js'
 describe('Marine License Project Details Card Component', () => {
   let $component
 
-  describe('With Change links', () => {
-    beforeEach(() => {
-      $component = renderComponent('marine-licence/project-details-card', {
-        projectName: 'Test Marine Project',
-        isReadOnly: false,
-        isApplicantView: true
-      })
+  beforeEach(() => {
+    $component = renderComponent('marine-licence/project-details-card', {
+      projectName: 'Test Marine Project'
     })
+  })
 
-    test('Should render project details card component', () => {
-      expect($component('#project-details-card')).toHaveLength(1)
-    })
+  test('Should render project details card component', () => {
+    expect($component('#project-details-card')).toHaveLength(1)
+  })
 
-    test('Should display project name', () => {
-      const htmlContent = $component.html()
-      expect(htmlContent).toContain('Test Marine Project')
-    })
+  test('Should display project name', () => {
+    const htmlContent = $component.html()
+    expect(htmlContent).toContain('Test Marine Project')
+  })
 
-    test('Should have correct card title', () => {
-      expect($component('.govuk-summary-card__title').text().trim()).toBe(
-        'Project summary'
-      )
-    })
+  test('Should have correct card title', () => {
+    expect($component('.govuk-summary-card__title').text().trim()).toBe(
+      'Project summary'
+    )
   })
 })

012b4af536f792d4fb94ee1f496430e5b64a2019
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-03-20 14:16:19 +0000

    Update spellings

--- src/server/internal-user-admin/exemptions/landing/controller.js
+++ src/server/internal-user-admin/exemptions/landing/controller.js
@@ -9,21 +9,9 @@ export const adminExemptionsController = {
     pre: [validateTeamAdminSession]
   },
   handler: async (request, h) => {
-    try {
-      return h.view(DASHBOARD_VIEW_ROUTE, {
-        pageTitle: DASHBOARD_PAGE_TITLE,
-        heading: DASHBOARD_PAGE_TITLE
-      })
-    } catch (error) {
-      request.logger.error(
-        { err: error },
-        'Error rendering internal admin page'
-      )
-
-      return h.view(DASHBOARD_VIEW_ROUTE, {
-        pageTitle: DASHBOARD_PAGE_TITLE,
-        heading: DASHBOARD_PAGE_TITLE
-      })
-    }
+    return h.view(DASHBOARD_VIEW_ROUTE, {
+      pageTitle: DASHBOARD_PAGE_TITLE,
+      heading: DASHBOARD_PAGE_TITLE
+    })
   }
 }

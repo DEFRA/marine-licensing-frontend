8dae9950511e35e76b5e9c26ed1c17b2694fac61
Russ Wicks <russell.wicks@equalexperts.com>
2026-04-02 22:46:59 +0100

    initial commit

--- src/server/common/constants/routes.js
+++ src/server/common/constants/routes.js
@@ -47,6 +47,7 @@ export const marineLicenceRoutes = {
   MARINE_LICENCE_DELETE: '/marine-licence/delete',
   MARINE_LICENCE_SPECIAL_LEGAL_POWERS: '/marine-licence/special-legal-powers',
   MARINE_LICENCE_OTHER_AUTHORITIES: '/marine-licence/other-authorities',
+  MARINE_LICENCE_PROJECT_BACKGROUND: '/marine-licence/project-background',
   MARINE_LICENCE_SITE_DETAILS: '/marine-licence/site-details',
   MARINE_LICENCE_COORDINATES_TYPE_CHOICE:
     '/marine-licence/how-do-you-want-to-provide-the-coordinates',

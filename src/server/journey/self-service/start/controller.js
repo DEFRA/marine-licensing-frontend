ea3983946e7975a7ecec5bb2d84aa7914495ea4d
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-04-09 20:10:04 +0100

    feat(ml-1162): add IAT start page controller

---
+++ src/server/journey/self-service/start/controller.js
@@ -0,0 +1,17 @@
+export const iatStartController = {
+  handler(_request, h) {
+    return h.view('journey/self-service/start/index', {
+      pageTitle: 'Check if you need a marine licence',
+      hidePhaseBanner: true,
+      links: {
+        jurisdiction:
+          'https://www.gov.uk/guidance/marine-licensing-definitions#jurisdiction',
+        exemptions:
+          'https://www.gov.uk/guidance/do-i-need-a-marine-licence#exemptions',
+        selfService:
+          'https://www.gov.uk/guidance/do-i-need-a-marine-licence#self-service',
+        guidance: 'https://www.gov.uk/guidance/do-i-need-a-marine-licence'
+      }
+    })
+  }
+}

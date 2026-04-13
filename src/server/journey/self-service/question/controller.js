b75563fe9e5b40dabf10c7a62a460e5427fca3dd
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-04-13 15:42:45 +0100

    refactor(ml-1186): use statusCodes constant for 404 in question controller

--- src/server/journey/self-service/question/controller.js
+++ src/server/journey/self-service/question/controller.js
@@ -3,6 +3,7 @@ import {
   getSection
 } from '#src/server/journey/self-service/services/journey-data.js'
 import { getBackLink } from '#src/server/journey/self-service/services/journey-history.js'
+import { statusCodes } from '#src/server/common/constants/status-codes.js'
 
 const VIEW_PATH = 'journey/self-service/question/index'
 
@@ -12,7 +13,7 @@ export const questionController = {
     const question = getQuestion(questionRoute)
 
     if (!question) {
-      return h.response('Not found').code(404)
+      return h.response('Not found').code(statusCodes.notFound)
     }
 
     const section = question.section ? getSection(question.section) : null

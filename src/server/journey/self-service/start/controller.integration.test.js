10be9fb0689258628abc1b6cb5113047b02d508f
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-04-15 15:37:01 +0100

    test(ml-1163): add integration tests for answer tracking and pre-selection

--- src/server/journey/self-service/start/controller.integration.test.js
+++ src/server/journey/self-service/start/controller.integration.test.js
@@ -1,7 +1,10 @@
 import { JSDOM } from 'jsdom'
 import { statusCodes } from '#src/server/common/constants/status-codes.js'
 import { setupTestServer } from '#tests/integration/shared/test-setup-helpers.js'
-import { makeGetRequest } from '#src/server/test-helpers/server-requests.js'
+import {
+  makeGetRequest,
+  makePostRequest
+} from '#src/server/test-helpers/server-requests.js'
 import { config } from '#src/config/config.js'
 
 describe('#iatStartController (integration)', () => {
@@ -91,5 +94,15 @@ describe('#iatStartController (integration)', () => {
   test('Renders no navigation links in the header', async () => {
     const { document } = await getPage()
     expect(document.querySelector('.govuk-service-navigation__list')).toBeNull()
+  })
+
+  test('POST redirects to first question', async () => {
+    const response = await makePostRequest({
+      url: '/journey/self-service/start',
+      server: getServer(),
+      formData: {}
+    })
+    expect(response.statusCode).toBe(statusCodes.redirect)
+    expect(response.headers.location).toBe('/journey/self-service/sea')
   })
 })

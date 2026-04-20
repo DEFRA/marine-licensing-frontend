9fb51532fd843817da5d49c8ee195b018ff53eaa
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-04-20 13:51:19 +0100

    feat(ml-1164): register intermediate outcome plugin

---
+++ src/server/journey/self-service/outcome/index.js
@@ -0,0 +1,35 @@
+import Joi from 'joi'
+import {
+  outcomeController,
+  outcomePostController
+} from '#src/server/journey/self-service/outcome/controller.js'
+import { routes } from '#src/server/common/constants/routes.js'
+
+export const journeySelfServiceOutcome = {
+  plugin: {
+    name: 'journeySelfServiceOutcome',
+    register(server) {
+      server.route([
+        {
+          method: 'GET',
+          path: routes.IAT_OUTCOME,
+          options: { auth: false },
+          ...outcomeController
+        },
+        {
+          method: 'POST',
+          path: routes.IAT_OUTCOME,
+          options: {
+            auth: false,
+            validate: {
+              payload: Joi.object({
+                outcomeType: Joi.string().required()
+              })
+            }
+          },
+          ...outcomePostController
+        }
+      ])
+    }
+  }
+}

eedca5f7328174fc435d2ded5306d882c9489905
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-04-21 15:00:56 +0100

    Make changes as per Sonar review

--- src/server/common/validation/select-activity/constants.js
+++ src/server/common/validation/select-activity/constants.js
@@ -1,19 +1,18 @@
-export const selectActivityErrorMessages = (activityType) => {
-  if (activityType === 'construction') {
-    return {
-      ACTIVITIES_REQUIRED: 'Select at least one type of structure',
-      ACTIVITIES_OTHER_REASON_REQUIRED: 'Enter details of the other structures'
-    }
-  } else if (activityType === 'deposit') {
-    return {
-      ACTIVITIES_REQUIRED: 'Select at least one type of substance or object',
-      ACTIVITIES_OTHER_REASON_REQUIRED: 'Enter details of the other deposits'
-    }
-  } else if (activityType === 'removal') {
-    return {
-      ACTIVITIES_REQUIRED: 'Select at least one substance or object',
-      ACTIVITIES_OTHER_REASON_REQUIRED:
-        'Enter details of the other substances or objects'
-    }
+const activityErrorMessages = {
+  construction: {
+    ACTIVITIES_REQUIRED: 'Select at least one type of structure',
+    ACTIVITIES_OTHER_REASON_REQUIRED: 'Enter details of the other structures'
+  },
+  deposit: {
+    ACTIVITIES_REQUIRED: 'Select at least one type of substance or object',
+    ACTIVITIES_OTHER_REASON_REQUIRED: 'Enter details of the other deposits'
+  },
+  removal: {
+    ACTIVITIES_REQUIRED: 'Select at least one substance or object',
+    ACTIVITIES_OTHER_REASON_REQUIRED:
+      'Enter details of the other substances or objects'
   }
 }
+
+export const selectActivityErrorMessages = (activityType) =>
+  activityErrorMessages[activityType]

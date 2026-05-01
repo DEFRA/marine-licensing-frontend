769daaec96496e5feebc0b409c9f6b0307a018a6
Vasudev Ganesan <vasudev.ganesan@esynergy.co.uk>
2026-05-01 15:44:15 +0100

    Update controller.js

--- src/server/internal-user-admin/exemptions/reports/controller.js
+++ src/server/internal-user-admin/exemptions/reports/controller.js
@@ -1,7 +1,8 @@
 import { authenticatedGetRequest } from '#src/server/common/helpers/authenticated-requests.js'
 import { validateTeamAdminSession } from '#src/server/common/helpers/user-session-validators.js'
 
-export const DASHBOARD_VIEW_ROUTE = 'internal-user-admin/exemptions/reports/index.njk'
+export const DASHBOARD_VIEW_ROUTE =
+  'internal-user-admin/exemptions/reports/index.njk'
 const DASHBOARD_PAGE_TITLE = 'Exemptions summary report'
 
 const mapSummaryReport = (value) => ({
@@ -16,7 +17,10 @@ export const adminReportsController = {
   },
   handler: async (request, h) => {
     try {
-      const { payload } = await authenticatedGetRequest(request, '/exemptions/summary')
+      const { payload } = await authenticatedGetRequest(
+        request,
+        '/exemptions/summary'
+      )
       return h.view(DASHBOARD_VIEW_ROUTE, {
         pageTitle: DASHBOARD_PAGE_TITLE,
         heading: DASHBOARD_PAGE_TITLE,

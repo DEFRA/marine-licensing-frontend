2cf5fe2e2809d61108bed8353da2738f60ce5826
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-07-01 11:31:45 +0100

    feat(ml-1248): link task-list considerations task to policy list page

--- src/server/marine-licence/task-list/controller.test.js
+++ src/server/marine-licence/task-list/controller.test.js
@@ -164,7 +164,7 @@ describe('#taskListController', () => {
 
     const mockMarinePlanPoliciesTaskList = [
       {
-        href: marineLicenceRoutes.MARINE_LICENCE_TASK_LIST,
+        href: marineLicenceRoutes.MARINE_LICENCE_MARINE_PLAN_POLICIES,
         status: {
           tag: { text: 'Not yet started', classes: 'govuk-tag--blue' }
         },

0d4e607846340e6def66a0a152061c2e156ad565
Russ Wicks <russell.wicks@equalexperts.com>
2026-07-03 10:21:56 +0100

    fixed merge issue

--- src/server/marine-licence/task-list/utils.js
+++ src/server/marine-licence/task-list/utils.js
@@ -187,7 +187,7 @@ export const transformMarinePlanPoliciesTaskList = (
         classes: taskClasses
       },
       href: marineLicenceRoutes.MARINE_LICENCE_MARINE_PLAN_POLICIES,
-      status: { tag: { text: 'Not yet started', classes: 'govuk-tag--blue' } }
+      status
     }
   ]
 }

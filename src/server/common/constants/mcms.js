d11297df0148c4923ced1358a590c98255138842
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-07-31 14:33:33 +0100

    Use config for variable

--- src/server/common/constants/mcms.js
+++ src/server/common/constants/mcms.js
@@ -1,2 +1,8 @@
-export const MCMS_LOGIN_URL =
-  'https://marinelicensing.marinemanagement.org.uk/mmofox5/fox/live/MMO_LOGIN/login'
+import { config } from '#src/config/config.js'
+
+const MCMS_LOGIN_PATH = 'mmofox5/fox/live/MMO_LOGIN/login'
+
+export const MCMS_LOGIN_URL = new URL(
+  MCMS_LOGIN_PATH,
+  config.get('mcms.url')
+).toString()

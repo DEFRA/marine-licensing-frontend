cea734b622bb515cb95fb5f78d196c0a60207331
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-08-04 11:40:23 +0100

    Unable to progress status

--- src/server/dashboard/utils.js
+++ src/server/dashboard/utils.js
@@ -4,7 +4,10 @@ import {
   marineLicenceRoutes
 } from '#src/server/common/constants/routes.js'
 import { EXEMPTION_TYPE } from '#src/server/common/constants/exemptions.js'
-import { PROJECT_STATUS } from '#src/server/common/constants/projects.js'
+import {
+  PROJECT_STATUS,
+  UNABLE_TO_PROGRESS
+} from '#src/server/common/constants/projects.js'
 import { getTagStyle } from '#src/server/common/helpers/ui/get-tag-style.js'
 import escapeHtml from 'lodash/escape.js'
 import {
@@ -87,6 +90,14 @@ export const getActionButtons = (project) => {
     : `<a href="${viewRoute}/${project.id}" class="govuk-link govuk-link--no-visited-state" aria-label="View details of ${escapedProjectName}">View details</a>`
 }
 
+export const getStatusLabelText = (status) => {
+  if (status === PROJECT_STATUS.REJECTED) {
+    return UNABLE_TO_PROGRESS
+  }
+
+  return escapeHtml(status)
+}
+
 export const formatProjectsForDisplay = (projects, isEmployee = false) =>
   projects.map((project) => {
     const { status, projectType } = project
@@ -103,7 +114,7 @@ export const formatProjectsForDisplay = (projects, isEmployee = false) =>
       },
       { text: project.applicationReference || '-' },
       {
-        html: `<strong class="govuk-tag ${getTagStyle(status)}">${escapeHtml(project.status)}</strong>`,
+        html: `<strong class="govuk-tag ${getTagStyle(status)}">${getStatusLabelText(project.status)}</strong>`,
         attributes: {
           'data-sort-value': project.status
         }
--- src/server/marine-licence/view-details/utils.js
+++ src/server/marine-licence/view-details/utils.js
@@ -4,14 +4,25 @@ import { PROJECT_STATUS } from '#src/server/common/constants/projects.js'
 import escapeHtml from 'lodash/escape.js'
 
 export const buildApplicationDetailsCardData = (marineLicence) => {
-  const { applicationReference, status, submittedAt, transferredDate } =
-    marineLicence
+  const {
+    applicationReference,
+    status,
+    submittedAt,
+    rejectedDate,
+    rejectedReasons,
+    rejectedInformation,
+    transferredDate
+  } = marineLicence
 
   return {
     applicationReference,
     submittedAt: formatDate(submittedAt, 'd MMM yyyy'),
     transferredDate: formatDate(transferredDate, 'd MMM yyyy'),
+    rejectedDate: formatDate(rejectedDate, 'd MMM yyyy'),
+    rejectedReasons: rejectedReasons ? rejectedReasons.split(',') : rejectedReasons,
+    rejectedInformation,
     isTransferred: status === PROJECT_STATUS.TRANSFERRED,
+    isRejected: status === PROJECT_STATUS.REJECTED,
     statusTag: `<strong class="govuk-tag ${getTagStyle(status)}">${escapeHtml(status)}</strong>`
   }
 }

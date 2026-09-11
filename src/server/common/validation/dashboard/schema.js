cc89d35d607c6f02302f29408605f551d1df2d12
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2026-09-11 10:28:26 +0100

    ML-1488: lcml dashboard filter pane (#896)
    * ML 1488 lcml dashboard filter pane (#884)
    
    * Install and Implement the filter ui
    
    * Style and UI for the Filter
    
    * Full filter functionality
    
    * Remove old filter
    
    * Resolve sonar
    
    * More sonar resolutions
    
    * No javascript clear link
    
    * test submit guard
    
    * Missing coverage
    
    * Add POST schema
    
    * ML-1488: LCML dashboard filter pane status and type (#895)
    
    * Install and Implement the filter ui
    
    * Style and UI for the Filter
    
    * Full filter functionality
    
    * Remove old filter
    
    * Resolve sonar
    
    * More sonar resolutions
    
    * No javascript clear link
    
    * test submit guard
    
    * Missing coverage
    
    * Status filter
    
    * Filter by type
    
    * Add POST schema
    
    * Update schema
    
    * Feature flagging
    
    * Run format
    
    * Sonar improvements
    
    * Add missing coverage
    
    * missing coverage
    
    * Review updates
    
    * Resolve after merge
    
    * ML-1488  Filter by users (#898)
    
    * Install and Implement the filter ui
    
    * Style and UI for the Filter
    
    * Full filter functionality
    
    * Remove old filter
    
    * Resolve sonar
    
    * More sonar resolutions
    
    * No javascript clear link
    
    * test submit guard
    
    * Missing coverage
    
    * Status filter
    
    * Filter by type
    
    * Add POST schema
    
    * Update schema
    
    * Feature flagging
    
    * Run format
    
    * Sonar improvements
    
    * Add missing coverage
    
    * missing coverage
    
    * Review updates
    
    * Filter by user options
    
    * Resolve after merge
    
    * Extract user names on the front end and add to query
    
    * cache users
    
    * do not show when no options are present
    
    * Fix no-js edge case with user selection
    
    * UI Updates
    
    * Update tests after UI change
    
    * fix slow loading issue
    
    * Update display for individual users
    
    * guard against user cache
    
    * Error output when no users selected
    
    * Fix tests
    
    * Tidy UI
    
    * Do not show only one option on specific user list
    
    * Design review improvements

---
+++ src/server/common/validation/dashboard/schema.js
@@ -0,0 +1,22 @@
+import joi from 'joi'
+import {
+  PROJECT_STATUS,
+  PROJECT_TYPE
+} from '#src/server/common/constants/projects.js'
+
+export const dashboardFilterSchema = joi.object({
+  show: joi.string().valid('all-projects', 'my-projects', 'specific-user'),
+  user: joi.when('show', {
+    is: 'specific-user',
+    then: joi.array().items(joi.string().uuid()).single(),
+    otherwise: joi.any().strip()
+  }),
+  status: joi
+    .array()
+    .items(joi.string().valid(...Object.keys(PROJECT_STATUS)))
+    .single(),
+  type: joi
+    .array()
+    .items(joi.string().valid(...Object.values(PROJECT_TYPE)))
+    .single()
+})
